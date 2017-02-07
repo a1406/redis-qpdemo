892efd82811dfb3a1cdf3550af6146d900c64023
jacktang <jack at zzfs>
2017-02-07 18:27:50 +0800

    1

--- redis-3.2.3/src/game_srv.c
+++ redis-3.2.3/src/game_srv.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "anet.h"
+
+int connect_conn_srv()
+{
+	return anetTcpConnect(NULL, "127.0.0.1", 7778);
+}
+
 int gamesrv_main(int argc, char *argv[])
 {
+	connect_conn_srv();
 	return (0);
 }
 
